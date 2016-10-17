e30732ee3d18f235972ef1ede827ef4caf69512e
Val <vmiha@yahoo-inc.com>
2016-10-16 17:54:17 -0700

    initial commit

---
+++ app/src/main/java/com/codepath/flickster/models/Movie.java
@@ -0,0 +1,62 @@
+package com.codepath.flickster.models;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+/**
+ * Created by vmiha on 10/16/16.
+ */
+public class Movie {
+
+    String posterPath;
+    String originalTitle;
+    String overview;
+
+    public String getPosterPath() {
+        return String.format("https://image.tmdb.org/t/p/w342/%s", posterPath);
+    }
+
+    public void setPosterPath(String posterPath) {
+        this.posterPath = posterPath;
+    }
+
+    public String getOriginalTitle() {
+        return originalTitle;
+    }
+
+    public void setOriginalTitle(String originalTitle) {
+        this.originalTitle = originalTitle;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+
+    public void setOverview(String overview) {
+        this.overview = overview;
+    }
+
+    public Movie(JSONObject jsonObject) throws JSONException {
+        this.posterPath = jsonObject.getString("poster_path");
+        this.originalTitle = jsonObject.getString("original_title");
+        this.overview = jsonObject.getString("overview");
+    }
+
+    public static ArrayList<Movie> fromJSONArray(JSONArray array){
+        ArrayList<Movie> results = new ArrayList<>();
+
+        for (int i = 0; i< array.length(); i++) {
+            try {
+                results.add(new Movie(array.getJSONObject(i)));
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        
+        return results;
+    }
+
+}
